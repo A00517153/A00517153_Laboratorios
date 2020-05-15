4c7901c3dceb20e3de60c04e44726341660a7bc1
A00517153 <A00517153@itesm.mx>
2020-05-15 17:30:36 +0000

    commit

---
+++ Laboratorio 15Mayo2020/main.cpp
@@ -0,0 +1,15 @@
+/*----------------------------------
+ *
+ * Laboratorio: POO y C++
+ * Fecha: 15-Mayo-2020
+ * Autor: A00517153 Johann Palomino
+ *
+ *----------------------------------*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  return 0;
+}
